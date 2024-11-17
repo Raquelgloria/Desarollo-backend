7a313a4131bbc57d059212309afe3a51b8c3a90d
JIvan013 <seraphyn4@gmail.com>
2024-11-17 15:32:22 -0600

    borrado
    borrado

--- workspace/sprintboot-service-celulares/src/main/java/com/microservice/celulares/respository/CelularDao.java
+++
@@ -1,9 +0,0 @@
-package com.microservice.celulares.respository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.microservice.celulares.entity.Celular;
-
-public interface CelularDao extends CrudRepository<Celular, Long> {
-
-}
--- workspace/sprintboot-service-celulares/src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO celulares (name, marca, create_at) VALUES ('Samsung', 'S21', NOW());
-INSERT INTO celulares (name, marca, create_at) VALUES ('Samsung', 'S22', NOW());
-INSERT INTO celulares (name, marca, create_at) VALUES ('Samsung', 'S23', NOW());
-INSERT INTO celulares (name, marca, create_at) VALUES ('Apple', 'Iphone 14', NOW());
-INSERT INTO celulares (name, marca, create_at) VALUES ('Apple', 'Iphone 15', NOW());
-INSERT INTO celulares (name, marca, create_at) VALUES ('Xiaomi', 'Mi', NOW());
